e53a86f90197df41cd2142d548830158181a17bc
Deepak jaat <deepakjhajhria4657@gmail.com>
2023-12-16 09:28:50 +0530

    update

--- src/Components/Sec1.jsx
+++ src/Components/Sec1.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 const Sec1 = () => {
   return (
-    <div className="container-xxl h-screen d-flex position-relative z-3">
+    <Container className="h-screen d-flex position-relative z-3">
       <div className="my-auto">
         <div className="d-flex flex-column">
           <p className="m-0 text-white text-7xl fw-normal ff-p">Vitae nunc,</p>
@@ -10,7 +11,7 @@ const Sec1 = () => {
         <p className="m-0 text-white max-547 lh-lg text-xl fw-medium ff-p pb-2 pb-sm-3 pb-md-4  pb-lg-5 pt-2 opacity-75"> Semper in egestas risus id tempus. Pellentesque dolor eros sollicitudin feugiat ut odio semper. Mattis fermentum eget ut vestibulum dolor diam</p>
         <button className="btn bg-white text-black rounded-pill  mt-2 mb-2 text-3xl fw-medium ff-p" href="#">Get Started</button>
       </div>
-    </div>
+    </Container>
   );
 };
 
